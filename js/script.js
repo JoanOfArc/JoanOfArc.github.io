f6600681aee796e40dc939197bca56a979c93f3d
Joanne <joannenhl@hotmail.com>
2023-10-02 16:02:18 +0100

    making split in header video

--- js/script.js
+++ js/script.js
@@ -65,8 +65,8 @@ function reset() {
         "Fly",
         "Soar",
         "Glide",
-        "Fly",
-        "Go"
+        "Go",
+        "We are Airbus"
       ],
       i = 0;
     setInterval(function () {
@@ -77,4 +77,21 @@ function reset() {
       });
     }, 2000);
 })();
+
+/* Animations on scroll */
+
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        entry.target.classList.add('image-animation');
+      }
+    });
+  });
   
+  const viewbox = document.querySelectorAll('.section-content');
+  viewbox.forEach(image => {
+    observer.observe(image);
+  });
